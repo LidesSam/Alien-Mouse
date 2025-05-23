c1b9fd85410fea71669bd4df6c27a47ccd9e3e29
unknown <lides.sam@gmail.com>
2025-05-23 20:33:37 -0300

    interface: prototype gameover popup menus

--- source/scenes/gameroom.gd
+++ source/scenes/gameroom.gd
@@ -10,3 +10,16 @@ func _ready():
 	player.set_tilemap(tilemap)
 	player.awaiting = false
 	pass
+
+
+func  gameover():
+	$gameover.show()
+
+func _on_back_btn_pressed() -> void:
+	ScreenTransition.change_scene_to_file("res://source/scenes/mainMenu.tscn")
+	pass # Replace with function body.
+
+
+func _on_replay_btn_pressed() -> void:
+	ScreenTransition.change_scene_to_file("res://source/scenes/gameroom.tscn")
+	pass # Replace with function body.
